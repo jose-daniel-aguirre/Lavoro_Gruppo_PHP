3282520a905a2d331b5efd7ac884f5d1b15b40b4
Stefano-Chiriac1 <160712038+Stefano-Chiriac1@users.noreply.github.com>
2024-04-14 21:43:33 +0200

    Cambiato tutto il game PHP ed è quasi finito
    Sto gruppo è propio una cagata sto a fa tutto il php da solo, ora mi sgozzo .

--- Game.php
+++ Game.php
@@ -9,16 +9,16 @@
 <body>
     <h1>HANGMAN GAME!!!!</h1>
     <?php
-    /*function debug_to_console($data) {
+    function debug_to_console($data) {
         $output = $data;
         if (is_array($output))
             $output = implode(',', $output);
     
         echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
     }
-    */
+
     session_start();
-    $imageNumber = 1;
+    $Lettera ;
     $x =1;
     $filename ="Settings.txt";
     $handler = fopen($filename,"r") or die("unable to open file!");
@@ -34,17 +34,21 @@
         }
     }
     fclose($handler);
-    $Player = [];
-    for ($i = 0; $i < $_SESSION["Quantity"]; $i++) // Partita
-{
-        $Fine=false;
-        echo "<h2>Round: ". $i + 1 . "</h2>";
-        $words = [];
-        $splitted = [];
-        $word;
-        $wordlength;
-        $index = 0;
-        $randword;
+    //variabili sessione
+    $_SESSION["round"] = 1;
+    $_SESSION["Punti"] = $Punti= [];
+    $_SESSION["Immagine"] = 1;
+    $_SESSION["incorso"] = false;
+
+    echo "<h2>Round: ". $_SESSION["round"] . "</h2>";
+    $Fine=false;
+    $words = [];
+    $index = 0;
+
+    
+    if ($_SESSION["incorso"] == false)
+    {// semplicemente pick
+
         $filename = "Paniere.txt";
         $handler = fopen($filename,"r") or die("unable to open file!");
         while (!feof($handler)) {
@@ -52,42 +56,38 @@
             $index++;
         }
         fclose($handler);
-        $randword = rand(0, count($words) -1);
-        $word = $words[$randword];
-        $splitted=str_split($word); // semplicemente pick
-        $wordlength = strlen($word) -2;
-        echo "<h3>Length: ". $wordlength. "</h3>"; // lunghezza parola
-        while ($Fine) //round
+    $_SESSION["Random"] = rand(0, count($words) -1);
+    $_SESSION["Parola"] = $words[$_SESSION["Random"]];
+    $_SESSION["Lunghezza"] = strlen($_SESSION["Parola"] )-2;
+    $_SESSION["wordS"] = str_split($_SESSION["Parola"]) ;
+    $_SESSION["incorso"] = true ;
+    }
+    else 
     {
-        for ($k = 0; $k = 1; $k++) // turno
+        debug_to_console("ti trovi nel else");
+    }
+
+    echo "<h3>Length: ". $_SESSION["Lunghezza"]. "</h3>"; // lunghezza parola
+        if ($_SERVER["REQUEST_METHOD"] == "POST") 
         {
-            if ($_SERVER["REQUEST_METHOD"] == "POST") 
-            {
             $Lettera = $_POST["Lettera"];
-            $k++;
-            echo "<h2> palle </h2>";
-            for ($j = 0; $j < $wordlength; $j++) //controllo
+            for ($j = 0; $j < $_SESSION["Lunghezza"]; $j++) //controllo
             {
-                if($splitted[$j] = $Lettera)
+                if($_SESSION["wordS[$j]"] = $Lettera)
                 {
-                    echo $Lettera;
+                    
                 }
                 else 
                 {
-                    $imageNumber++;
+                    $_SESSION["Immagine"]++;
                 }
-            }   
             }
         }
-        $Fine = true;
-    }
-}
-
     ?>
-    <img src="<?php echo $imageNumber; ?>.jpg"><br>
+    <img src="<?php echo $_SESSION["Immagine"]; ?>.jpg"><br>
     <form action="Game.php" method="post">
-    <input type="text" maxlength="1" id="Lettera" onkeydown="return /[a-z]/i.test(event.key)" ><br><br>
-    <input type="text" id="Lettera" onkeydown="return /[a-z]/i.test(event.key)" ><br><br>
+    <input type="text" maxlength="1" id="Lettera" name="Lettera" onkeydown="return /[a-z]/i.test(event.key)" ><br><br>
+    <!--<input type="text" id="Letter" onkeydown="return /[a-z]/i.test(event.key)" ><br><br> -->
     <input type="submit" value="Conferma">
 </body>
 </html>
